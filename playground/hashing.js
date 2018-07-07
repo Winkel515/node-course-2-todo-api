f53b73bd18147fcf51db496e4a3d9f4a0ec02801
Winkel Yin <winkel515@hotmail.com>
2018-07-07 09:33:38 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,32 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc';
 
-var token = jwt.sign({
-    data: 'Shit'
-}, '123abc');
-var decoded = jwt.verify(token, '123abc');
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+})
 
-console.log(token);
-console.log(decoded);
+var hashPassword = '$2a$10$HmYsi46jPGRxE0MCFXD.mu7pfcFaxm7E5OMhZt1/jk/77UhgHtF4.';
+
+bcrypt.compare(password, hashPassword, (err, res) => {
+    console.log(res);
+})
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign({
+//     data: 'Shit'
+// }, '123abc');
+// var decoded = jwt.verify(token, '123abc');
+
+// console.log(token);
+// console.log(decoded);
 
 // var message = 'I am user number 3';
 // var hash  = SHA256(message).toString();
