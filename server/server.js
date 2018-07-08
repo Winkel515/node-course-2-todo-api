ef1f80873c2f55f8479e487f224fa76f99a0746d
Winkel Yin <winkel515@hotmail.com>
2018-07-08 12:20:35 -0400

    Add DELETE /users/me/logout route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,12 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 })
 
+app.delete('/users/me/logout', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }).catch((e) => res.status(400).send());
+})
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
