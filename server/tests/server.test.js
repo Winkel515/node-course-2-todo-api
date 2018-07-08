f8a59f8e4b28a600ba10849f3a5adf0c5ef0531d
Winkel Yin <winkel515@hotmail.com>
2018-07-08 12:36:31 -0400

    Add tests for DELETE /users/me/logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -281,3 +281,24 @@ describe('POST /users/login', () => {
             });
     })
 })
+
+describe('DELETE /users/me/logout', () => {
+    it('should remove the auth token on logout', (done) => {
+        var token = users[0].tokens[0].token
+
+        request(app)
+            .delete('/users/me/logout')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+                
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    })
+})
